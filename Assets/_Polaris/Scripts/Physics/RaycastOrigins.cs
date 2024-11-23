d10963608d82ec3d99e26ef0b0431754a55e70e3
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2024-11-23 02:11:56 -0600

    refactor(physics): moved raycast controller project content

---
+++ Assets/_Polaris/Scripts/Physics/RaycastOrigins.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Polaris.Physics
+{
+    public struct RaycastOrigins
+    {
+        public Vector2 topLeft;
+        public Vector2 topRight;
+        public Vector2 bottomLeft;
+        public Vector2 bottomRight;
+    }
+}
